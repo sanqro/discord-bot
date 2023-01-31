4823ca99bc243584f66a35fcbc6c5c4c15731829
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-01-31 11:35:22 +0100

    add mongoose to project and implement warn command

--- client/commands/index.js
+++ client/commands/index.js
@@ -1,3 +1,4 @@
 export { default as ping } from "./ping.js";
 export { default as user } from "./users.js";
 export { default as server } from "./server.js";
+export { default as warn } from "./warn.js";
---
+++ client/commands/warn.js
@@ -0,0 +1,51 @@
+import { PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import mongoose from "mongoose";
+
+// The following code for mongoose was sourced from this url:
+// https://www.geeksforgeeks.org/how-to-connect-node-js-to-a-mongodb-database/
+
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://localhost:27017/discordbot", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const warningSchema = {
+    target: String,
+    reason: String
+};
+
+const Warning = mongoose.model("warnings", warningSchema);
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("warn")
+        .setDescription("Warns  the user you enter as a target")
+        .addUserOption((option) =>
+            option
+                .setName("target")
+                .setDescription("Warns the user you enter as a target")
+                .setRequired(true)
+        )
+        .addStringOption((option) =>
+            option
+                .setName("reason")
+                .setDescription("You can optionally provide a reason for the warn here.")
+                .setRequired(false)
+        )
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+    async execute(interaction) {
+        const target = interaction.options.getUser("target");
+        const reason = interaction.options.getString("reason") ?? "No reason specified.";
+
+        const warning = new Warning({
+            target,
+            reason
+        });
+
+        // save warning data do the database
+        await warning.save();
+
+        await interaction.reply(`Warned ${target.username} for: ${reason}`);
+    }
+};
